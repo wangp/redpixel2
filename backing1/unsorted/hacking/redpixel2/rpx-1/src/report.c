38a1bbc8dd1787db25559dc27e15d3af7d033b8f
Peter Wang <novalazy@gmail.com>
1999-04-25 18:11:37 -0500

    Import backing1/unsorted/hacking/redpixel2/rpx-1/src/report.c

---
+++ backing1/unsorted/hacking/redpixel2/rpx-1/src/report.c
@@ -0,0 +1,24 @@
+/* report.c : Error reporting functions */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <allegro.h>
+
+//#ifdef Xwin_ALLEGRO
+
+void errprintf(char *fmt, ...)	       /* FIXME, dos can't use this */
+{
+    char buf[256];
+    va_list ap;
+    va_start(ap, fmt);
+    vsprintf(buf, fmt, ap);
+    va_end(ap);
+    
+    printf("Error: %s", buf);
+}
+
+//#else				       
+
+
+
+//#endif
