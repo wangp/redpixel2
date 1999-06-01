84477270583f7b03df5dd58a70797b0991f48a3c
Peter Wang <novalazy@gmail.com>
1999-06-01 11:45:25 -0500

    Import backing1/unsorted/hacking/redpixel2/rpx-1/src/defs.h

---
+++ backing1/unsorted/hacking/redpixel2/rpx-1/src/defs.h
@@ -0,0 +1,17 @@
+/* defs.h : some #defines */
+#ifndef _included_defs_h_
+#define _included_defs_h_
+
+#define TILE_W 16
+#define TILE_H TILE_W
+
+
+#define RPX_MAX_WIDTH	256	       /* MASSIVE!! */
+#define RPX_MAX_HEIGHT	256
+
+
+/* messages */
+
+typedef unsigned int msg_t;	       /* $-export */
+
+#endif
