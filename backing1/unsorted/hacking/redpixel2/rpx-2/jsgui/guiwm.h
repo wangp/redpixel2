41ce44a542a80c4dde56454e0e209fc546dedbc7
Peter Wang <novalazy@gmail.com>
2000-06-09 23:20:29 -0500

    Import backing1/unsorted/hacking/redpixel2/rpx-2/jsgui/guiwm.h

---
+++ backing1/unsorted/hacking/redpixel2/rpx-2/jsgui/guiwm.h
@@ -0,0 +1,15 @@
+#ifndef __included_guiwm_h
+#define __included_guiwm_h
+
+
+/*
+ * 	The WM code (guiwm.c) has to fit in with the look
+ * 	maintained by the drawing routines (guidraw.c). 
+ * 	This file maintains a consistent Look & Feel(tm).
+ */
+
+
+#define WM_TITLEBAR_H	20
+
+
+#endif /* __included_guiwm_h */
