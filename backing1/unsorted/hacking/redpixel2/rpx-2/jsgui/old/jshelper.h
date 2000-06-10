13f47217958d7be7c512dcb82368632491aeb256
Peter Wang <novalazy@gmail.com>
2000-06-09 23:20:32 -0500

    Import backing1/unsorted/hacking/redpixel2/rpx-2/jsgui/old/jshelper.h

---
+++ backing1/unsorted/hacking/redpixel2/rpx-2/jsgui/old/jshelper.h
@@ -0,0 +1,6 @@
+#ifndef _included_jshelper_h_
+#define _included_jshelper_h_
+
+JSBool JS_GetSubProperty(JSContext *cx, JSObject *obj, const char *name, jsval *vp);
+
+#endif /* _included_jshelper_h_ */
