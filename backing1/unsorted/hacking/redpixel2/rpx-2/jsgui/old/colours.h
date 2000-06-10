00a4269cea2b8d256258cbd56acb827fb6dfb92f
Peter Wang <novalazy@gmail.com>
2000-06-09 23:20:32 -0500

    Import backing1/unsorted/hacking/redpixel2/rpx-2/jsgui/old/colours.h

---
+++ backing1/unsorted/hacking/redpixel2/rpx-2/jsgui/old/colours.h
@@ -0,0 +1,13 @@
+#ifndef _included_colours_h_
+#define _included_colours_h_
+
+extern int paw_black;
+extern int paw_blue;
+extern int paw_red;
+extern int paw_green;
+extern int paw_lightgrey;
+extern int paw_darkgrey;
+
+void init_thepaw_colours();
+
+#endif /* _included_colours_h_ */
