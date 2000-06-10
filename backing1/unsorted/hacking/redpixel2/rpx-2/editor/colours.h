c1eb591fecddb26cf774d041557c49515b7ebf66
Peter Wang <novalazy@gmail.com>
2000-06-09 23:20:36 -0500

    Import backing1/unsorted/hacking/redpixel2/rpx-2/editor/colours.h

---
+++ backing1/unsorted/hacking/redpixel2/rpx-2/editor/colours.h
@@ -0,0 +1,13 @@
+#ifndef __included_colours_h
+#define __included_colours_h
+
+extern int paw_black;
+extern int paw_blue;
+extern int paw_red;
+extern int paw_green;
+extern int paw_lightgrey;
+extern int paw_darkgrey;
+
+void init_thepaw_colours();
+
+#endif /* __included_colours_h */
