4acfa924205139f7590155b6f28badc3d6eec465
Peter Wang <novalazy@gmail.com>
2000-11-25 18:19:54 -0500

    Import backing2/redstone/oldstuff/dataold/script/old/crate.lua

---
+++ backing2/redstone/oldstuff/dataold/script/old/crate.lua
@@ -0,0 +1,6 @@
+-- crate.lua
+
+store_load ("object/crate.dat", "/crate/")
+objtype_register ("objtile", "crate-1", "/crate/crate-1")
+objtype_register ("objtile", "crate-2", "/crate/crate-2")
+objtype_register ("objtile", "crate-big", "/crate/crate-big")
