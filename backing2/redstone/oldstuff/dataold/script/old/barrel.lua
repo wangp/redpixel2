99c93e33849a86b9c9d044e16053b3a0ebddbc37
Peter Wang <novalazy@gmail.com>
2000-11-25 18:02:43 -0500

    Import backing2/redstone/oldstuff/dataold/script/old/barrel.lua

---
+++ backing2/redstone/oldstuff/dataold/script/old/barrel.lua
@@ -0,0 +1,5 @@
+-- barrel.lua
+
+store_load ("object/barrel.dat", "/barrel/")
+objtype_register ("objtile", "barrel-grey", "/barrel/barrel-grey")
+objtype_register ("objtile", "barrel-red", "/barrel/barrel-red")
