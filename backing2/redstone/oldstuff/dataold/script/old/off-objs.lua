366b7fd089748918f519298366823f71e2b8bcd0
Peter Wang <novalazy@gmail.com>
2001-04-01 10:50:13 -0500

    Import backing2/redstone/oldstuff/dataold/script/old/off-objs.lua

---
+++ backing2/redstone/oldstuff/dataold/script/old/off-objs.lua
@@ -0,0 +1,9 @@
+-- off-objs.lua
+
+store_load ("object/off-objs.dat", "/office-objects/")
+objtype_register ("objtile", "monitor-front", "/office-objects/monitor-anim1")
+objtype_register ("objtile", "monitor-left", "/office-objects/monitor-left")
+objtype_register ("objtile", "monitor-right", "/office-objects/monitor-right")
+objtype_register ("objtile", "brain", "/office-objects/brain")
+objtype_register ("objtile", "printer", "/office-objects/printer")
+objtype_register ("objtile", "tv", "/office-objects/tv-anim1")
