a9c63c1ced0ca60ab73fe0b22e3b17faa182366f
Peter Wang <novalazy@gmail.com>
2001-04-07 12:02:51 -0500

    Import backing2/redstone/oldstuff/dataold/script/old/armour.lua

---
+++ backing2/redstone/oldstuff/dataold/script/old/armour.lua
@@ -0,0 +1,6 @@
+-- armour.lua
+
+store_load ("object/armour.dat", "/armour/")
+objtype_register ("item", "armour-brown", "/armour/armour-brown")
+objtype_register ("item", "armour-purple", "/armour/armour-purple")
+objtype_register ("item", "armour-blue", "/armour/armour-blue")
