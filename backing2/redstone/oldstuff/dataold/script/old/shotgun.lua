1fbf9d5b9f904fd2c822ff3811e5327d2260d02c
Peter Wang <novalazy@gmail.com>
2001-04-07 11:58:13 -0500

    Import backing2/redstone/oldstuff/dataold/script/old/shotgun.lua

---
+++ backing2/redstone/oldstuff/dataold/script/old/shotgun.lua
@@ -0,0 +1,5 @@
+-- shotgun.lua
+
+store_load ("object/shotgun.dat", "/weapon/shotgun/")
+objtype_register ("item", "shotgun", "/weapon/shotgun/pickup")
+objtype_register ("item", "shotgun-shell", "/weapon/shotgun/ammo")
