75bfccc7e4c6ef01a960a8e7ee8352966baf99dd
Peter Wang <novalazy@gmail.com>
2001-04-09 10:26:26 -0500

    Import backing2/redstone/oldstuff/dataold/script/offc-obj.lua

---
+++ backing2/redstone/oldstuff/dataold/script/offc-obj.lua
@@ -0,0 +1,12 @@
+-- offc-obj.lua
+
+-- Module init.
+
+store_load ("object/offc-obj.dat", "/office-objects/")
+objtype_register ("objtile", "video-camera",	"/office-objects/camera-anim-000")
+objtype_register ("objtile", "cpu",		"/office-objects/cpu")
+objtype_register ("objtile", "monitor-front",	"/office-objects/monitor-anim-000")
+objtype_register ("objtile", "monitor-left",	"/office-objects/monitor-left")
+objtype_register ("objtile", "monitor-right",	"/office-objects/monitor-right")
+objtype_register ("objtile", "printer",		"/office-objects/printer")
+objtype_register ("objtile", "tv",		"/office-objects/tv-anim-000")
