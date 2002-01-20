770d03905ab12e34e33cd4dafea3b7d3e3ac56c1
Peter Wang <novalazy@gmail.com>
2002-01-20 06:58:54 +0000

    Some initial code to make crates destructible.
    git-svn-id: file:///home/tjaden/{repo}/redstone/trunk@85 ec8ac367-a008-0410-9901-8f3136df4969

--- data/basic/basic-tilelike.lua
+++ data/basic/basic-tilelike.lua
@@ -4,9 +4,46 @@ store_load ("basic/basic-tilelike.dat", "/basic/tilelike/")
 
 objtype_register ("objtile", "basic-barrel-red", "/basic/tilelike/barrel000")
 objtype_register ("objtile", "basic-barrel-grey", "/basic/tilelike/barrel001")
-objtype_register ("objtile", "basic-crate-000", "/basic/tilelike/crate-normal000")
-objtype_register ("objtile", "basic-crate-001", "/basic/tilelike/crate-normal001")
-objtype_register ("objtile", "basic-crate-large-000", "/basic/tilelike/crate-large000")
+
+
+
+----------------------------------------------------------------------
+--  Crates
+----------------------------------------------------------------------
+
+local make_crate_nonproxy_init
+function make_crate_nonproxy_init (health, respawn_secs)
+    return function (self)
+	self.health = health
+	function self:receive_damage (amount)
+	    self.health = self.health - amount
+	    if self.health <= 0 then
+		self:hide_and_respawn_later (respawn_secs * 1000)
+	    end
+	end
+    end
+end
+
+Objtype {
+    category = "objtile",
+    name = "basic-crate-000",
+    icon = "/basic/tilelike/crate-normal000",
+    nonproxy_init = make_crate_nonproxy_init (15, 20)
+}
+
+Objtype {
+    category = "objtile",
+    name = "basic-crate-001",
+    icon = "/basic/tilelike/crate-normal001",
+    nonproxy_init = make_crate_nonproxy_init (15, 20)
+}
+
+Objtype {
+    category = "objtile",
+    name = "basic-crate-large-000",
+    icon = "/basic/tilelike/crate-large000",
+    nonproxy_init = make_crate_nonproxy_init (30, 20)
+}
 
 
 ----------------------------------------------------------------------
