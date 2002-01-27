2eb0fcf902cc2881a06e67b54f33993da14b32f2
Peter Wang <novalazy@gmail.com>
2002-01-27 14:05:09 +0000

    Initial revision
    git-svn-id: file:///home/tjaden/{repo}/redstone/trunk@155 ec8ac367-a008-0410-9901-8f3136df4969

---
+++ src/blast.h
@@ -0,0 +1,18 @@
+#ifndef __included_blast_h
+#define __included_blast_h
+
+
+struct list_head;
+struct BITMAP;
+
+
+typedef struct blast blast_t;
+
+
+blast_t *blast_create (float x, float y, float radius, int damage, int visual_only);
+void blast_destroy (blast_t *blast);
+int blast_update (blast_t *blast, struct list_head *object_list);
+void blast_draw (struct BITMAP *dest, blast_t *blast, int offset_x, int offset_y);
+
+
+#endif
