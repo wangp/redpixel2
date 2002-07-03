99170314356006f1098a1fe48253d020853b60b5
Peter Wang <novalazy@gmail.com>
2002-07-03 07:45:59 +0000

    (oops)
    git-svn-id: file:///home/tjaden/{repo}/redstone/trunk@211 ec8ac367-a008-0410-9901-8f3136df4969

--- src/store/store.h
+++ src/store/store.h
@@ -22,14 +22,14 @@ void store_shutdown (void);
 store_file_t store_load (AL_CONST char *filename, AL_CONST char *prefix);
 store_file_t store_load_ex (AL_CONST char *filename, AL_CONST char *prefix,
 			    DATAFILE *(*loader) (AL_CONST char *));
-void store_unload (store_file_t id);
+void store_unload (store_file_t f);
 
 store_index_t store_get_index (AL_CONST char *key);
 AL_CONST char *store_get_key (store_index_t index);
 DATAFILE *store_get_datafile (AL_CONST char *key);
 void *store_get_dat (AL_CONST char *key);
 
-DATAFILE *store_get_file (store_file_t id);
+DATAFILE *store_get_file (store_file_t f);
 
 #ifdef __cplusplus
 }
