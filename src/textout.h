e176b3c9c6d3aac17e5095de0d059310dcadeeec
Peter Wang <novalazy@gmail.com>
2002-07-10 03:53:51 +0000

    Refactored textout.c and completed missing functions for completeness.
    git-svn-id: file:///home/tjaden/{repo}/redstone/trunk@236 ec8ac367-a008-0410-9901-8f3136df4969

--- src/textout.h
+++ src/textout.h
@@ -10,11 +10,21 @@ void textout_magic (
     struct BITMAP *, struct FONT *, const char *, int x, int y, int color);
 void textout_right_magic (
     struct BITMAP *, struct FONT *, const char *, int x, int y, int color);
+void textout_centre_magic (
+    struct BITMAP *, struct FONT *, const char *, int x, int y, int color);
+
+void textout_trans_magic (
+    struct BITMAP *, struct FONT *, const char *, int x, int y, int color);
+void textout_right_trans_magic (
+    struct BITMAP *, struct FONT *, const char *, int x, int y, int color);
 void textout_centre_trans_magic (
     struct BITMAP *, struct FONT *, const char *, int x, int y, int color);
+
 void textprintf_trans_magic (
     struct BITMAP *, struct FONT *, int x, int y, int col, const char *, ...);
 void textprintf_right_trans_magic (
+    struct BITMAP *, struct FONT *, int x, int y, int col, const char *, ...);
+void textprintf_centre_trans_magic (
     struct BITMAP *, struct FONT *, int x, int y, int col, const char *, ...);
 
 
