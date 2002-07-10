19fd535cca16b04f5c126833ce66904070314648
Peter Wang <novalazy@gmail.com>
2002-07-10 03:53:40 +0000

    Refactor textout.c and completed missing functions for completeness.
    git-svn-id: file:///home/tjaden/{repo}/redstone/trunk@235 ec8ac367-a008-0410-9901-8f3136df4969

--- src/textout.c
+++ src/textout.c
@@ -1,4 +1,4 @@
-/* textout.c
+/* textout.c -- text routines for magic bitmaps
  * 
  * Peter Wang <tjaden@users.sourceforge.net>
  */
@@ -9,75 +9,66 @@
 #include "textout.h"
 
 
-void textout_magic (BITMAP *bmp, FONT *font, const char *buf,
-		    int x, int y, int color)
+enum { LEFT, RIGHT, CENTRE };
+
+static void _do_it (
+    BITMAP *bmp, FONT *font, const char *buf,
+    int x, int y, int color,
+    void (*drawer) (BITMAP *, BITMAP *, int, int),
+    int align)
 {
-    int len = text_length (font, buf);
-    BITMAP *tmp = create_magic_bitmap (len, text_height (font));
-    clear_bitmap (tmp);
-    textout (tmp, font, buf, 0, 0, color);
-    draw_magic_sprite (bmp, tmp, x, y);
-    destroy_bitmap (tmp);
-}
-
-
-void textout_right_magic (BITMAP *bmp, FONT *font, const char *buf,
-			  int x, int y, int color)
-{
-    int len = text_length (font, buf);
-    BITMAP *tmp = create_magic_bitmap (len, text_height (font));
-    clear_bitmap (tmp);
-    textout (tmp, font, buf, 0, 0, color);
-    draw_trans_magic_sprite (bmp, tmp, x - len, y);
-    destroy_bitmap (tmp);
-}
-
-
-void textout_centre_trans_magic (BITMAP *bmp, FONT *font, const char *s,
-				 int x, int y, int color)
-{
-    BITMAP *tmp = create_magic_bitmap (text_length (font, s),
-				       text_height (font));
-    clear_bitmap (tmp);
-    textout (tmp, font, s, 0, 0, color);
-    draw_trans_magic_sprite (bmp, tmp, x - tmp->w/3/2, y);
-    destroy_bitmap (tmp);
-}
-
-
-void textprintf_trans_magic (BITMAP *bmp, FONT *font, int x, int y,
-			     int color, const char *fmt, ...)
-{
-    va_list ap;
+    int len;
     BITMAP *tmp;
-    char buf[1024];
 
-    va_start (ap, fmt);
-    uvszprintf (buf, sizeof buf, fmt, ap);
-    va_end (ap);
-
-    tmp = create_magic_bitmap (text_length (font, buf), text_height (font));
+    len = text_length (font, buf);
+    tmp = create_magic_bitmap (len, text_height (font));
     clear_bitmap (tmp);
     textout (tmp, font, buf, 0, 0, color);
-    draw_trans_magic_sprite (bmp, tmp, x, y);
+
+    switch (align) {
+	case LEFT:   drawer (bmp, tmp, x, y); break;
+	case RIGHT:  drawer (bmp, tmp, x - len, y); break;
+	case CENTRE: drawer (bmp, tmp, x - tmp->w/3/2, y); break;
+    }
+
     destroy_bitmap (tmp);
 }
 
 
-void textprintf_right_trans_magic (BITMAP *bmp, FONT *font, int x, int y,
-				   int color, const char *fmt, ...)
-{
-    va_list ap;
-    BITMAP *tmp;
-    char buf[1024];
+/* plain string */
 
-    va_start (ap, fmt);
-    uvszprintf (buf, sizeof buf, fmt, ap);
-    va_end (ap);
-
-    tmp = create_magic_bitmap (text_length (font, buf), text_height (font));
-    clear_bitmap (tmp);
-    textout (tmp, font, buf, 0, 0, color);
-    draw_trans_magic_sprite (bmp, tmp, x - tmp->w/3, y);
-    destroy_bitmap (tmp);
+#define MK_THINGO(NAME, DRAWER, ALIGN)				\
+void NAME (BITMAP *bmp, FONT *font, const char *buf,		\
+	   int x, int y, int color)				\
+{								\
+    _do_it (bmp, font, buf, x, y, color, DRAWER, ALIGN);	\
 }
+
+MK_THINGO(textout_magic, draw_magic_sprite, LEFT)
+MK_THINGO(textout_right_magic, draw_magic_sprite, RIGHT)
+MK_THINGO(textout_centre_magic, draw_magic_sprite, CENTRE)
+
+MK_THINGO(textout_trans_magic, draw_trans_magic_sprite, LEFT)
+MK_THINGO(textout_right_trans_magic, draw_trans_magic_sprite, RIGHT)
+MK_THINGO(textout_centre_trans_magic, draw_trans_magic_sprite, CENTRE)
+
+
+/* format string */
+
+#define MK_THINGO_F(NAME, DRAWER, ALIGN)			\
+void NAME (BITMAP *bmp, FONT *font, int x, int y,		\
+	   int color, const char *fmt, ...)			\
+{								\
+    va_list ap;							\
+    char buf[1024];						\
+								\
+    va_start (ap, fmt);						\
+    uvszprintf (buf, sizeof buf, fmt, ap);			\
+    va_end (ap);						\
+								\
+    _do_it (bmp, font, buf, x, y, color, DRAWER, ALIGN);	\
+}
+
+MK_THINGO_F (textprintf_trans_magic, draw_trans_magic_sprite, LEFT)
+MK_THINGO_F (textprintf_right_trans_magic, draw_trans_magic_sprite, RIGHT)
+MK_THINGO_F (textprintf_centre_trans_magic, draw_trans_magic_sprite, CENTRE)
