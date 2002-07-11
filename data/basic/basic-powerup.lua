de1d7a284d25d7ebe6901967f73dc5cde499765e
Peter Wang <novalazy@gmail.com>
2002-07-11 15:20:34 +0000

    Made health items not disappear unless used.
    git-svn-id: file:///home/tjaden/{repo}/redstone/trunk@264 ec8ac367-a008-0410-9901-8f3136df4969

--- data/basic/basic-powerup.lua
+++ data/basic/basic-powerup.lua
@@ -62,7 +62,9 @@ Item {
 local Health_Item = function (t)
     return Item (t, {
 	collide_hook = function (self, player)
+	    local h = player.health
 	    player:receive_health (t.give_health)
+	    return player.health == h
 	end
     })
 end
