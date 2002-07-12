ca1d11020d788a20bb102ba23fa4ab4581fe9f01
Peter Wang <novalazy@gmail.com>
2002-07-12 17:46:20 +0000

    Made lightamps animate.
    git-svn-id: file:///home/tjaden/{repo}/redstone/trunk@287 ec8ac367-a008-0410-9901-8f3136df4969

--- data/basic/basic-powerup.lua
+++ data/basic/basic-powerup.lua
@@ -41,6 +41,16 @@ Item {
     respawn_secs = 10
 }
 
+local lightamp_anim = {
+    "/basic/powerup/lightamp/000",
+    "/basic/powerup/lightamp/000",
+    "/basic/powerup/lightamp/001",
+    "/basic/powerup/lightamp/002",
+    "/basic/powerup/lightamp/003",
+    "/basic/powerup/lightamp/002",
+    "/basic/powerup/lightamp/001"
+}
+
 Item {
     name = "basic-lightamp", 
     icon = "/basic/powerup/lightamp/000", 
@@ -51,6 +61,18 @@ Item {
     end,
     proxy_init = function (self)
 	self:add_light ("/basic/light/blue-16", 0, 0)
+
+	self.frame = 1
+	self:set_update_hook (
+	    250 + random (50),
+	    function (self)
+		self.frame = self.frame + 1
+		if self.frame > getn (lightamp_anim) then
+		    self.frame = 1
+		end
+		self:replace_layer (0, lightamp_anim[self.frame], 7, 3)
+	    end
+	)
     end
 }
 
