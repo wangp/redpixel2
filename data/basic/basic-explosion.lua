bd016901ceb38019fb3b6c7ba245b0c4de15e2ac
Peter Wang <novalazy@gmail.com>
2002-07-15 14:35:22 +0000

    Added basic-explo20.
    git-svn-id: file:///home/tjaden/{repo}/redstone/trunk@330 ec8ac367-a008-0410-9901-8f3136df4969

--- data/basic/basic-explosion.lua
+++ data/basic/basic-explosion.lua
@@ -3,6 +3,13 @@
 store_load ("basic/basic-explosion.dat", "/basic/explosion/")
 
 explosion_type_register (
+    "basic-explo20",
+    "/basic/explosion/explo20/000", 16, 40/16,
+    "/basic/light/white-16",
+    "/basic/explosion/explo42/sound" --XXX
+)
+
+explosion_type_register (
     "basic-explo42",
     "/basic/explosion/explo42/000", 17, 50/17,
     "/basic/light/white-16",
